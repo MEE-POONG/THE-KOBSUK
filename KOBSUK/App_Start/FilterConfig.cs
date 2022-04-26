44c880df5df46af51c6e52c7b5452cd17d4edf6f
Warayut Taekrathok <warayut2555@gmail.com>
2022-04-26 14:20:50 +0700

    Initial commit

---
+++ KOBSUK/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace KOBSUK
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
